16996cdfc7875b4990e94733427e6d2c9bb41f9d
Zeynep Ozturkmen <Zeynep Ozturkmen@LAPTOP-C6541CSF>
2021-04-10 20:37:40 +0300

    Added data layer

---
+++ EducationProject.Data/DbContexts/EducationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EducationProject.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationProject.Data.DbContexts
+{
+    public class EducationDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public EducationDbContext(DbContextOptions<EducationDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ EducationProject.Data/DbContexts/EducationDbSeed.cs
@@ -0,0 +1,71 @@
+﻿using EducationProject.Core.Constants;
+using EducationProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EducationProject.Data.DbContexts
+{
+    public static class EducationDbSeed
+    {
+        private static void GetAndAddEnums<TEntity, TEnum>(this DbContext dbContext, bool create,
+      IDictionary<TEnum, Guid> ids) where TEntity : BaseEnum
+        {
+            foreach (TEnum e in Enum.GetValues(typeof(TEnum)))
+            {
+
+                var name = e.ToString();
+                var query = dbContext.Set<TEntity>().SingleOrDefault(q => q.Name == name);
+                if (query != null)
+                {
+                    ids.Add(e, query.Id);
+                }
+                else if (create)
+                {
+                    var memberInfo = typeof(TEnum).GetMember(name)[0];
+                    var hasDisplayAttribute = Attribute.IsDefined(memberInfo, typeof(DisplayAttribute));
+                    var displayAttribute =
+                        hasDisplayAttribute ? memberInfo.GetCustomAttribute<DisplayAttribute>() : null;
+
+                    if (displayAttribute?.Description != null)
+                    {
+                        var newEntity =
+                            (TEntity)Activator.CreateInstance(typeof(TEntity), name, displayAttribute.Description);
+                        newEntity.RecordDate = DateTime.Now;
+                        var entity = dbContext.Set<TEntity>().Add(newEntity).Entity;
+                        dbContext.SaveChanges();
+                        ids.Add(e, entity.Id);
+                    }
+                    else
+                    {
+                        var newEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), name);
+                        var entity = dbContext.Set<TEntity>().Add(newEntity).Entity;
+                        dbContext.SaveChanges();
+                        ids.Add(e, entity.Id);
+                    }
+                }
+            }
+        }
+
+
+        public static async void Initialize(IServiceProvider serviceProvider, bool create = false)
+        {
+            using var serviceScope = serviceProvider.CreateScope();
+            var dbContext = serviceScope.ServiceProvider.GetService<EducationDbContext>();
+
+            dbContext.Database.Migrate();
+
+            dbContext.GetAndAddEnums<Category, Constants.Category>(create, Constants.CategoryIds);
+            dbContext.GetAndAddEnums<TeacherInformation, Constants.TeacherInformation>(create, Constants.TeacherInformationIds);
+            dbContext.GetAndAddEnums<EducationContentType, Constants.EducationContentType>(create, Constants.EducationContentTypeIds);
+            dbContext.GetAndAddEnums<UserEducationStatus, Constants.UserEducationStatus>(create, Constants.UserEducationStatusIds);
+          
+        }
+    }
+
+}
